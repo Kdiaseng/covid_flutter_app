8752bb9ec07ad7eb4d5a0a0e07391c7560b8fcfd
kaleb.silva <kaleb.silva@itbam.org.br>
2021-07-05 15:24:43 -0400

    refactor: change card info and applay color

--- lib/app/modules/countries/components/item_info_loading.dart
+++ lib/app/modules/countries/components/item_info_loading.dart
@@ -6,45 +6,35 @@ class ItemInfoLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (_, index) => _buildLoading(),
-        separatorBuilder: (_, index) => Container(height: 10),
-        itemCount: 100);
+    return _buildLoading();
   }
 
-  _buildLoading() => Container(
-        padding: const EdgeInsets.only(left: 16, right: 16),
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
-              child: Material(
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(8),
-                    bottomRight: Radius.circular(8)),
-                elevation: 4,
-                child: Padding(
-                  padding: const EdgeInsets.all(18.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 35),
-                        child: Skeleton(
-                            height: 15, width: 100, style: SkeletonStyle.text),
-                      ),
-                      Skeleton(height: 15, width: 50, style: SkeletonStyle.text)
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 70,
-              width: 70,
-              child: CircleAvatar(backgroundColor: Colors.grey.shade300),
-            )
-          ],
-        ),
-      );
+  _buildLoading() =>Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Material(
+      elevation: 2,
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildItemInfo(),
+          _buildItemInfo(),
+          _buildItemInfo(),
+          _buildItemInfo(),
+          _buildItemInfo(),
+          _buildItemInfo(),
+        ],
+      ),
+    ),
+  );
+
+  _buildItemInfo() => ListTile(
+    leading:Skeleton(
+        height: 35, width: 35, style: SkeletonStyle.circle),
+    title: Skeleton(
+        height: 20, width: 100, style: SkeletonStyle.text),
+    trailing: Skeleton(
+        height: 20, width: 50, style: SkeletonStyle.text)
+  );
 }
--- lib/app/modules/countries/country_details_page.dart
+++ lib/app/modules/countries/country_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:covid_flutter_app/app/modules/countries/components/item_info.dart';
 import 'package:covid_flutter_app/app/modules/countries/country_controller.dart';
+import 'package:covid_flutter_app/app/utils/icons_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -33,80 +34,79 @@ class _CountryDetailsPageState extends State<CountryDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: NestedScrollView(
-          controller: controller.scrollController,
-          headerSliverBuilder: (_, innerBoxIsScrolled) {
-            return [
-              Observer(
-                builder: (_) => SliverAppBar(
-                  expandedHeight: controller.kExpandedHeight,
-                  pinned: true,
-                  floating: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    collapseMode: CollapseMode.pin,
-                    centerTitle: false,
-                    titlePadding: EdgeInsets.symmetric(
-                        vertical: 16,
-                        horizontal: controller.horizontalTitlePadding),
-                    background: controller.isLoadCountrySelected
-                        ? Image.network(
-                            controller.countrySelected.countryInfo.flag,
-                            fit: BoxFit.cover)
-                        : Container(color: Colors.grey.shade300),
-                    title: controller.isLoadCountrySelected
-                        ? Text(controller.countrySelected.country)
-                        : Skeleton(
-                            height: 15, width: 100, style: SkeletonStyle.text),
-                  ),
+      body: NestedScrollView(
+        controller: controller.scrollController,
+        headerSliverBuilder: (_, innerBoxIsScrolled) {
+          return [
+            Observer(
+              builder: (_) => SliverAppBar(
+                expandedHeight: controller.kExpandedHeight,
+                pinned: true,
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                  centerTitle: false,
+                  titlePadding: EdgeInsets.symmetric(
+                      vertical: 16,
+                      horizontal: controller.horizontalTitlePadding),
+                  background: controller.isLoadCountrySelected
+                      ? Image.network(
+                          controller.countrySelected.countryInfo.flag,
+                          fit: BoxFit.cover)
+                      : Container(color: Colors.grey.shade300),
+                  title: controller.isLoadCountrySelected
+                      ? Text(controller.countrySelected.country)
+                      : Skeleton(
+                          height: 15, width: 100, style: SkeletonStyle.text),
                 ),
               ),
-            ];
-          },
-          body: Center(
-            child: Observer(
-              builder: (_) => controller.isLoadCountrySelected
-                  ? ListView(children: _buildInfoList())
-                  : ItemInfoLoading(),
             ),
+          ];
+        },
+        body: Center(
+          child: Observer(
+            builder: (_) => controller.isLoadCountrySelected
+                ? _buildCardInfo()
+                : ItemInfoLoading(),
           ),
         ),
       ),
     );
   }
 
-  List<Widget> _buildInfoList() {
+  _buildCardInfo() {
     final countrySelected = controller.countrySelected;
-    return [
-      ItemInfo(
-          title: "Population",
-          value: countrySelected.population.toString(),
-          iso3: countrySelected.countryInfo.iso3),
-      SizedBox(height: 10),
-      ItemInfo(
-          title: "Cases",
-          value: countrySelected.cases.toString(),
-          iso3: countrySelected.countryInfo.iso3),
-      SizedBox(height: 10),
-      ItemInfo(
-          title: "Deaths",
-          value: countrySelected.deaths.toString(),
-          iso3: countrySelected.countryInfo.iso3),
-      SizedBox(height: 10),
-      ItemInfo(
-          title: "Recovered",
-          value: countrySelected.recovered.toString(),
-          iso3: countrySelected.countryInfo.iso3),
-      SizedBox(height: 10),
-      ItemInfo(
-          title: "Tests",
-          value: countrySelected.tests.toString(),
-          iso3: countrySelected.countryInfo.iso3),
-      SizedBox(height: 10),
-      ItemInfo(
-          title: "Critical",
-          value: countrySelected.critical.toString(),
-          iso3: countrySelected.countryInfo.iso3)
-    ];
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Material(
+        elevation: 2,
+        borderRadius: BorderRadius.all(Radius.circular(16.0)),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildItemInfo('Population', countrySelected.population.toString()),
+            _buildItemInfo('Cases', countrySelected.cases.toString()),
+            _buildItemInfo('Deaths', countrySelected.deaths.toString()),
+            _buildItemInfo('Recovered', countrySelected.recovered.toString()),
+            _buildItemInfo('Tests', countrySelected.tests.toString()),
+            _buildItemInfo('Critical', countrySelected.critical.toString()),
+          ],
+        ),
+      ),
+    );
   }
+
+  _buildItemInfo(String title, String value) => ListTile(
+        leading:
+            Icon(CovIcons.coronavirus, color: controller.getColor(), size: 35),
+        title: Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.w400),
+        ),
+        trailing: Text(
+          value,
+          style: TextStyle(fontSize: 18),
+        ),
+      );
 }
